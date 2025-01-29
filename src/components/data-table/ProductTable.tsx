fe594394bd2b4fd37902d1f55d00cd10054bd831
alamin-cse44 <alamincse44@gmail.com>
2025-01-30 02:40:14 +0600

    Refactor: product table

--- src/components/data-table/ProductTable.tsx
+++ src/components/data-table/ProductTable.tsx
@@ -25,6 +25,7 @@ import { useDeleteBikesMutation, useGetAllBikesQuery } from "../../redux/feature
 import { Button, Fab } from "@mui/material";
 import EditIcon from "@mui/icons-material/Edit";
 import { toast } from "sonner";
+import { headCells } from "./ProductTableColumns";
 
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
   if (b[orderBy] < a[orderBy]) {
@@ -50,57 +51,7 @@ function getComparator<Key extends keyof any>(
     : (a, b) => -descendingComparator(a, b, orderBy);
 }
 
-interface HeadCell {
-  disablePadding: boolean;
-  id: keyof TBike;
-  label: string;
-  numeric: boolean;
-}
 
-const headCells: readonly HeadCell[] = [
-  {
-    id: "image",
-    numeric: false,
-    disablePadding: true,
-    label: "Image",
-  },
-  {
-    id: "name",
-    numeric: false,
-    disablePadding: true,
-    label: "Name",
-  },
-  {
-    id: "brand",
-    numeric: false,
-    disablePadding: false,
-    label: "Brand",
-  },
-  {
-    id: "model",
-    numeric: false,
-    disablePadding: false,
-    label: "Model",
-  },
-  {
-    id: "price",
-    numeric: true,
-    disablePadding: false,
-    label: "Price",
-  },
-  {
-    id: "quantity",
-    numeric: true,
-    disablePadding: false,
-    label: "Quantity",
-  },
-  // {
-  //   id: "action",
-  //   numeric: true,
-  //   disablePadding: false,
-  //   label: "Action",
-  // },
-];
 
 interface EnhancedTableProps {
   numSelected: number;
---
+++ src/components/data-table/ProductTableColumns.tsx
@@ -0,0 +1,53 @@
+import { TBike } from "../../types";
+
+interface HeadCell {
+    disablePadding: boolean;
+    id: keyof TBike;
+    label: string;
+    numeric: boolean;
+  }
+  
+  export const headCells: readonly HeadCell[] = [
+    {
+      id: "image",
+      numeric: false,
+      disablePadding: true,
+      label: "Image",
+    },
+    {
+      id: "name",
+      numeric: false,
+      disablePadding: true,
+      label: "Name",
+    },
+    {
+      id: "brand",
+      numeric: false,
+      disablePadding: false,
+      label: "Brand",
+    },
+    {
+      id: "model",
+      numeric: false,
+      disablePadding: false,
+      label: "Model",
+    },
+    {
+      id: "price",
+      numeric: true,
+      disablePadding: false,
+      label: "Price",
+    },
+    {
+      id: "quantity",
+      numeric: true,
+      disablePadding: false,
+      label: "Quantity",
+    },
+    // {
+    //   id: "action",
+    //   numeric: true,
+    //   disablePadding: false,
+    //   label: "Action",
+    // },
+  ];
