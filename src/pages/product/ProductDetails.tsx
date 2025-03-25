1ee689f8dec6caa6356a191043d74d9133ab2c14
alamin-cse44 <alamincse44@gmail.com>
2025-03-25 13:29:04 +0600

    Add: product details skeleton

--- src/pages/product/ProductDetails.tsx
+++ src/pages/product/ProductDetails.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Skeleton, Typography } from "@mui/material";
 import { Link, useParams } from "react-router-dom";
 import { useGetSignleBikeQuery } from "../../redux/features/bike/bikeApi";
 import { useAppSelector } from "../../redux/hooks";
@@ -8,7 +8,7 @@ import { toast } from "sonner";
 import { FaCartPlus } from "react-icons/fa";
 const ProductDetails = () => {
   const { id } = useParams();
-  const { data } = useGetSignleBikeQuery(id);
+  const { data, isLoading } = useGetSignleBikeQuery(id);
 
   const user = useAppSelector(selectCurrentUser);
   const [createCart] = useCreateCartMutation();
@@ -36,145 +36,176 @@ const ProductDetails = () => {
     <Container>
       {/* Left Section */}
       {!data?.data && <h2>Loading....</h2>}
-      <Box
+      <Typography
+        variant="h4"
         sx={{
-          display: "flex",
-          flexDirection: { xs: "column", md: "row" },
-          alignItems: "center",
-          justifyContent: "space-between",
-          p: 4,
-          gap: 4,
+          fontWeight: "bold",
+          my: 5,
+          fontSize: { xs: "18px", md: "35px" },
+          textAlign: "center",
         }}
       >
-        {/* Left Section */}
+        {isLoading ? (
+          <Skeleton width={400} />
+        ) : (
+          `Details of ${data?.data?.name}`
+        )}
+      </Typography>
+      {isLoading ? (
         <Box
           sx={{
-            flex: 1,
-            textAlign: { xs: "center", md: "left" },
-            color: "#1a1a1a",
+            display: "flex",
+            flexDirection: { xs: "column", md: "row" },
+            alignItems: "center",
+            justifyContent: "space-between",
+            p: 4,
+            gap: 4,
           }}
         >
-          <Typography
-            variant="h4"
-            fontWeight="bold"
-            gutterBottom
-            sx={{ color: "#333" }}
-          >
-            {data?.data?.name}
-          </Typography>
-          <Typography
-            variant="h6"
-            fontWeight="medium"
-            gutterBottom
-            sx={{ color: "#666" }}
-          >
-            Brand - {data?.data?.brand}
-          </Typography>
-          <Typography
-            variant="h6"
-            fontWeight="medium"
-            gutterBottom
-            sx={{ color: "#666" }}
-          >
-            Model - {data?.data?.model}
-          </Typography>
-          <Typography
-            variant="body1"
-            color="textSecondary"
-            sx={{
-              lineHeight: 1.6,
-              fontSize: "1rem",
-              color: "#555",
-              marginBottom: "1.5rem",
-            }}
-          >
-            Experience unmatched comfort and style with{" "}
-            <span style={{ color: "#ff5722" }}>{data?.data?.name}</span> .
-            Designed for performance, this premium footwear combines
-            cutting-edge technology with a sleek, modern look, making it perfect
-            for both athletes and trendsetters.
-          </Typography>
-          <Typography
-            variant="h5"
-            fontWeight="bold"
-            sx={{ color: "#ff5722", marginBottom: "1.5rem" }}
-          >
-            Stock Quantity - {data?.data?.quantity}
-          </Typography>
-          <Typography
-            variant="h5"
-            fontWeight="bold"
-            sx={{ color: "#ff5722", marginBottom: "1.5rem" }}
-          >
-            Price - ${data?.data?.price.toFixed(2)}
-          </Typography>
+          <Skeleton width={"100%"} height={400} />
+          <Skeleton width={"100%"} height={400} />
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: { xs: "column", md: "row" },
+            alignItems: "center",
+            justifyContent: "space-between",
+            p: 4,
+            gap: 4,
+          }}
+        >
+          {/* Left Section */}
           <Box
             sx={{
-              display: "flex",
-              flexDirection: { xs: "column", sm: "row" },
-              gap: 2,
-              justifyContent: { xs: "center", md: "flex-start" },
+              flex: 1,
+              textAlign: { xs: "center", md: "left" },
+              color: "#1a1a1a",
             }}
           >
-            <Button
-              onClick={handleAddToCart}
-              variant="outlined"
-              color="primary"
-              sx={{
-                px: 4,
-                py: 1.5,
-                borderColor: "#ff5722",
-                color: "#ff5722",
-                "&:hover": {
-                  borderColor: "#e64a19",
-                  color: "#e64a19",
-                },
-              }}
-              startIcon={<FaCartPlus color="" />}
+            <Typography
+              variant="h4"
+              fontWeight="bold"
+              gutterBottom
+              sx={{ color: "#333" }}
             >
-              Add to Cart
-            </Button>
-            <Button
-              component={Link}
-              to={`/single-checkout/${data?.data?._id}`}
-              variant="contained"
-              color="primary"
+              {data?.data?.name}
+            </Typography>
+            <Typography
+              variant="h6"
+              fontWeight="medium"
+              gutterBottom
+              sx={{ color: "#666" }}
+            >
+              Brand - {data?.data?.brand}
+            </Typography>
+            <Typography
+              variant="h6"
+              fontWeight="medium"
+              gutterBottom
+              sx={{ color: "#666" }}
+            >
+              Model - {data?.data?.model}
+            </Typography>
+            <Typography
+              variant="body1"
+              color="textSecondary"
               sx={{
-                px: 4,
-                py: 1.5,
-                color: "white",
-                backgroundColor: "#ff5722",
-                "&:hover": { backgroundColor: "#e64a19" },
+                lineHeight: 1.6,
+                fontSize: "1rem",
+                color: "#555",
+                marginBottom: "1.5rem",
               }}
             >
-              BUY NOW
-            </Button>
+              Experience unmatched comfort and style with{" "}
+              <span style={{ color: "#ff5722" }}>{data?.data?.name}</span> .
+              Designed for performance, this premium footwear combines
+              cutting-edge technology with a sleek, modern look, making it
+              perfect for both athletes and trendsetters.
+            </Typography>
+            <Typography
+              variant="h5"
+              fontWeight="bold"
+              sx={{ color: "#ff5722", marginBottom: "1.5rem" }}
+            >
+              Stock Quantity - {data?.data?.quantity}
+            </Typography>
+            <Typography
+              variant="h5"
+              fontWeight="bold"
+              sx={{ color: "#ff5722", marginBottom: "1.5rem" }}
+            >
+              Price - ${data?.data?.price.toFixed(2)}
+            </Typography>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: { xs: "column", sm: "row" },
+                gap: 2,
+                justifyContent: { xs: "center", md: "flex-start" },
+              }}
+            >
+              <Button
+                onClick={handleAddToCart}
+                variant="outlined"
+                color="primary"
+                sx={{
+                  px: 4,
+                  py: 1.5,
+                  borderColor: "#ff5722",
+                  color: "#ff5722",
+                  "&:hover": {
+                    borderColor: "#e64a19",
+                    color: "#e64a19",
+                  },
+                }}
+                startIcon={<FaCartPlus color="" />}
+              >
+                Add to Cart
+              </Button>
+              <Button
+                component={Link}
+                to={`/single-checkout/${data?.data?._id}`}
+                variant="contained"
+                color="primary"
+                sx={{
+                  px: 4,
+                  py: 1.5,
+                  color: "white",
+                  backgroundColor: "#ff5722",
+                  "&:hover": { backgroundColor: "#e64a19" },
+                }}
+              >
+                BUY NOW
+              </Button>
+            </Box>
+          </Box>
+
+          {/* Right Section */}
+          <Box
+            sx={{
+              flex: 1,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Box
+              component="img"
+              src={data?.data?.image}
+              alt="Fitness"
+              sx={{
+                width: "100%",
+                maxWidth: "400px",
+                height: "auto",
+                borderRadius: "16px",
+                boxShadow: 3,
+              }}
+            />
           </Box>
         </Box>
-
-        {/* Right Section */}
-        <Box
-          sx={{
-            flex: 1,
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Box
-            component="img"
-            src={data?.data?.image}
-            alt="Fitness"
-            sx={{
-              width: "100%",
-              maxWidth: "400px",
-              height: "auto",
-              borderRadius: "16px",
-              boxShadow: 3,
-            }}
-          />
-        </Box>
-      </Box>
+      )}
     </Container>
   );
 };
